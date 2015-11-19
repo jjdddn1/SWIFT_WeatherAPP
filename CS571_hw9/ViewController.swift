774acd7ec09dffb92302c0b3d8ff501e2d4b236e
喻嘉 <huiyuanren@huiyuandemacbook-air.local>
2015-11-18 16:22:15 -0800

    Initial Commit

---
+++ CS571_hw9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CS571_hw9
+//
+//  Created by Huiyuan Ren on 15/11/18.
+//  Copyright © 2015年 Huiyuan Ren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CS571_hw9Tests/CS571_hw9Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CS571_hw9Tests.swift
+//  CS571_hw9Tests
+//
+//  Created by Huiyuan Ren on 15/11/18.
+//  Copyright © 2015年 Huiyuan Ren. All rights reserved.
+//
+
+import XCTest
+@testable import CS571_hw9
+
+class CS571_hw9Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
